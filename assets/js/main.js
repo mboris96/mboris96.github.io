00ec497ae4de77f00736b3dfc519426769ad0688
Matt Boris <mboris@iruleathome.com>
2017-06-12 14:40:44 -0400

    Added loading progress indicator

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,18 +17,6 @@
 	});
 
 	$(function() {
-
-		$(window).load(function(){
-		$(window).scroll(function() {
-			var wintop = $(window).scrollTop(), docheight = $('article').height(), winheight = $(window).height();
-			console.log(wintop);
-			var totalScroll = (wintop/(docheight-winheight))*100;
-			console.log("total scroll" + totalScroll);
-			$(".KW_progressBar").css("width",totalScroll+"%");
-		});
-
-		});
-
 		var	$window = $(window),
 			$body = $('body'),
 			$header = $('#header'),
